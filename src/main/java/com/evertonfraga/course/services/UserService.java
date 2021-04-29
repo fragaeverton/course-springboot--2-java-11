e05b0507252bbe72df66135176b24de100579100
Everton Fraga <evertonfraga90@hotmail.com>
2021-04-29 21:34:38 +0200

    Exception handling - findById

--- src/main/java/com/evertonfraga/course/services/UserService.java
+++ src/main/java/com/evertonfraga/course/services/UserService.java
@@ -2,9 +2,11 @@ package com.evertonfraga.course.services;
 
 import com.evertonfraga.course.entities.User;
 import com.evertonfraga.course.repositories.UserRepository;
+import com.evertonfraga.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.ReadOnlyFileSystemException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +22,7 @@ public class UserService  {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow( () -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
